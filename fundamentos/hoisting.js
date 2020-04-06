0473642257316ef3117df3e4abf79bfd49245c55
Vinicius <vinibecke@gmail.com>
2020-04-06 17:04:13 -0300

    fundamentos

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,20 @@
+console.log('a =', a)
+
+var a = 2 // joga a declaração para cima
+
+console.log('a =', a)
+
+function teste(){
+    console.log('a =', a)
+
+    var a = 2 // joga a declaração para cima
+
+    console.log('a =', a)
+}
+
+teste()
+
+//console.log('b =', b) COM LET NAO DA 
+let b = 2
+
+console.log('b\ =', b)
